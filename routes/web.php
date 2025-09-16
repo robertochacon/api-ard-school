88dd23e4689dff23c18f70e7fa8caf128fbc8225
rchaconrv <hola.hack09@gmail.com>
2025-09-16 04:11:53 -0400

    redirect to documentation

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return redirect('/api/documentation');
+});
+
+// Welcome page (alternative route)
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
